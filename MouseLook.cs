8385b7ed94e07026ea6fa32eb7374c4a2e0804a7
Wyattbr <73440811+Wyattbr@users.noreply.github.com>
2020-10-25 12:26:55 -0600

    Add files via upload

---
+++ MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerbody;
+    public float MouseSens = 100f;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSens * Time.deltaTime;
+
+        playerbody.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+}
